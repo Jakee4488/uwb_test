ea21f2d2dcd9b6899772b0f6d9af5196a226c6b9
Jakee4488 <111643447+Jakee4488@users.noreply.github.com>
2023-03-16 12:00:39 +0000

    some files

--- app/src/main/java/com/example/uwb_tests/MainActivity.kt
+++ app/src/main/java/com/example/uwb_tests/MainActivity.kt
@@ -1,41 +1,106 @@
 package com.example.uwb_tests
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanSettings
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.wifi.ScanResult
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.uwb.RangingParameters
+import androidx.core.uwb.RangingResult
+import androidx.core.uwb.UwbManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {package com.example.uwb_test
+class MainActivity : AppCompatActivity() {
+    private lateinit var uwbManager: UwbManager
+    private val controllerSession by lazy { uwbManager.controllerSessionScope() }
+    private val controleeSession by lazy { uwbManager.controleeSessionScope() }
+    private lateinit var bluetoothManager: BluetoothManager
+    private lateinit var bluetoothLeScanner: BluetoothLeScanner
 
-    import android.bluetooth.BluetoothManager
-    import android.content.Context
-    import android.content.pm.PackageManager
-    import android.os.Bundle
-    import androidx.appcompat.app.AppCompatActivity
-
-    class MainActivity : AppCompatActivity() {
-        val packageManager: PackageManager = applicationContext.packageManager
-        private val REQUEST_BLUETOOTH_PERMISSION = 1
-        private val bluetoothManager =
-            getSystemService(Context.BLUETOOTH_SERVICE)
-                    as BluetoothManager
-        val bluetoothLeScanner =bluetoothManager.adapter.bluetoothLeScanner
-
-        bluetoothLeScanner.startScan(...,)
-
-        val deviceSupportUwb = packageManager.hasSystemFeature("andriod.hardware.uwb")
-
-
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-            if (deviceSupportUwb) {
-
-            }
-        }
-    }
-
+    private val REQUEST_BLUETOOTH_PERMISSION = 1
+    private val serviceUuid = "<serviceUuid>" //replace with actual service UUID
+    private val deviceAddress = "<deviceAddress>" //replace with actual device address
+    private val sessionKey = "<sessionKey>" //replace with actual session key
+    private val complexChannel = "<complexChannel>" //replace with actual channel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Check if the device has UWB hardware support
+        val deviceSupportUwb = packageManager.hasSystemFeature("android.hardware.uwb")
+
+        if (deviceSupportUwb) {
+            // Initialize UWB manager
+            uwbManager = UwbManager.createInstance(this)
+
+            // Check for Bluetooth permission
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.BLUETOOTH),
+                    REQUEST_BLUETOOTH_PERMISSION
+                )
+            } else {
+                // Initialize Bluetooth scanner
+                bluetoothManager =
+                    getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+                bluetoothLeScanner = bluetoothManager.adapter.bluetoothLeScanner
+
+                // Start scanning for devices
+                bluetoothLeScanner.startScan(
+                    null,
+                    ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                        .build(),
+                    object : ScanCallback() {
+                        override fun onScanResult(callbackType: Int, result: ScanResult) {
+                            super.onScanResult(callbackType, result)
+
+                            // Get the service data of the UWB device
+                            val serviceData = result.scanRecord?.getServiceData(serviceUuid)
+
+                            // Connect to the UWB device
+                            if (result.device.address == deviceAddress && serviceData != null) {
+                                connectToDevice(serviceData)
+                                bluetoothLeScanner.stopScan(this)
+                            }
+                        }
+                    })
+            }
+        }
     }
+
+    private fun connectToDevice(serviceData: ByteArray) {
+        // Create ranging parameters for the UWB session
+        val rangingParams = RangingParameters(
+            uwbConfigType = RangingParameters.UWB_CONFIG_ID_1,
+            sessionKeyInfo = sessionKey.toByteArray(),
+            complexChannel = complexChannel.toByte(),
+            peerDevices = listOf(deviceAddress),
+            updateRate = RangingParameters.RANGING_UPDATE_RATE_FREQUENT
+        )
+
+        // Prepare UWB session
+        val sessionFlow = if (uwbManager.isControleeSupported) {
+            controleeSession.prepareSession(rangingParams)
+        } else {
+            controllerSession.prepareSession(rangingParams)
+        }
+
+        //
+
+
 }
+
