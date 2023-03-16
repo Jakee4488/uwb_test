ae557e0d6b03e66c1d9c8f70cfa2baba4f2f5691
Jakee4488 <111643447+Jakee4488@users.noreply.github.com>
2023-03-16 11:17:56 +0000

    Update MainActivity.kt

--- app/src/main/java/com/example/uwb_tests/MainActivity.kt
+++ app/src/main/java/com/example/uwb_tests/MainActivity.kt
@@ -3,7 +3,37 @@ package com.example.uwb_tests
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {package com.example.uwb_test
+
+    import android.bluetooth.BluetoothManager
+    import android.content.Context
+    import android.content.pm.PackageManager
+    import android.os.Bundle
+    import androidx.appcompat.app.AppCompatActivity
+
+    class MainActivity : AppCompatActivity() {
+        val packageManager: PackageManager = applicationContext.packageManager
+        private val REQUEST_BLUETOOTH_PERMISSION = 1
+        private val bluetoothManager =
+            getSystemService(Context.BLUETOOTH_SERVICE)
+                    as BluetoothManager
+        val bluetoothLeScanner =bluetoothManager.adapter.bluetoothLeScanner
+
+        bluetoothLeScanner.startScan(...,)
+
+        val deviceSupportUwb = packageManager.hasSystemFeature("andriod.hardware.uwb")
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            if (deviceSupportUwb) {
+
+            }
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
